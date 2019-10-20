25853f7a0f591a550ebe545af7890bade8f2a5f0
SoulMonk <fsoulmonk@gmail.com>
2019-10-20 11:22:31 +0300

    formatting

--- pkg/db/pg/migration/1571546291912_init.sql
+++ pkg/db/pg/migration/1571546291912_init.sql
@@ -7,19 +7,19 @@ create table "user"
     email      varchar(255),
     password   varchar(255),
     salt       varchar(255),
-    enabled    boolean default false,
+    enabled    boolean   default false,
     created_at timestamp default now(),
     updated_at timestamp default now()
 );
 
 create table "user-verification"
 (
-    id     serial not null
+    id        serial  not null
         constraint user_verification_pk
             primary key,
-    user_id integer not null
+    user_id   integer not null
         references "user",
-    code   varchar(64),
+    code      varchar(64),
     activated boolean default false
 );
 
