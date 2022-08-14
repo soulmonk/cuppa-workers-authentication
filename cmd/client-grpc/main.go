4030a9654a0c7f671e84e3101db4a7f02dfd97b7
SoulMonk <fsoulmonk@gmail.com>
2022-08-14 14:57:27 +0200

    added cobra and survey simple grpc client cli

--- cmd/client-grpc/main.go
+++ cmd/client-grpc/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"flag"
 	"fmt"
 	"github.com/AlecAivazis/survey/v2"
 	"github.com/soulmonk/cuppa-workers-authentication/pkg/api/v1"
@@ -18,66 +17,102 @@ const (
 	apiVersion = "v1"
 )
 
+const (
+	usernameFlag = "username"
+	emailFlag    = "email"
+	serverFlag   = "server"
+)
+
 var rootCmd = &cobra.Command{
 	Use:   "client-grpc",
-	Short: "Hugo is a very fast static site generator",
+	Short: "simple cli to login, signup, validate token",
 	Long:  ``,
-	Run: func(cmd *cobra.Command, args []string) {
-		// Do Stuff Here
-	},
 }
 
 var loginCmd = &cobra.Command{
-	Use: "login",
+	Use:   "login",
+	Short: "login user",
+	Run: func(cmd *cobra.Command, args []string) {
+		username := cmd.Flag(usernameFlag).Value.String()
+		server := cmd.Flag(serverFlag).Value.String()
+
+		conn, ctx, cancel := createConnection(server)
+
+		defer conn.Close()
+		defer cancel()
+
+		doLogin(ctx, conn, username)
+	},
 }
+var signupCmd = &cobra.Command{
+	Use:   "signup",
+	Short: "signup user",
+	Run: func(cmd *cobra.Command, args []string) {
+		username := cmd.Flag(usernameFlag).Value.String()
+		email := cmd.Flag(emailFlag).Value.String()
+		server := cmd.Flag(serverFlag).Value.String()
 
-func Execute() {
+		conn, ctx, cancel := createConnection(server)
 
+		defer conn.Close()
+		defer cancel()
+
+		doSignUp(ctx, conn, username, email)
+	},
+}
+var validateTokenCmd = &cobra.Command{
+	Use:   "validate",
+	Short: "validate user token",
+	Run: func(cmd *cobra.Command, args []string) {
+		server := cmd.Flag(serverFlag).Value.String()
+
+		conn, ctx, cancel := createConnection(server)
+
+		defer conn.Close()
+		defer cancel()
+
+		doTokenValidate(ctx, conn)
+	},
 }
 
 func main() {
+	rootCmd.PersistentFlags().StringP(serverFlag, "s", ":9090", "gRPC server in format host:port")
+
+	loginCmd.Flags().StringP(usernameFlag, "u", "", "username")
+	_ = loginCmd.MarkFlagRequired(usernameFlag)
+
+	signupCmd.Flags().StringP(usernameFlag, "u", "", "username")
+	signupCmd.Flags().StringP(emailFlag, "e", "", "email")
+	_ = signupCmd.MarkFlagRequired(usernameFlag)
+	_ = signupCmd.MarkFlagRequired(emailFlag)
+
+	rootCmd.AddCommand(loginCmd)
+	rootCmd.AddCommand(signupCmd)
+	rootCmd.AddCommand(validateTokenCmd)
+
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-	// get configuration
-	address := flag.String("server", ":9090", "gRPC server in format host:port")
-	username := flag.String("username", "", "username")
-	email := flag.String("email", "", "used in signup")
-	action := flag.String("action", "", "action")
-	flag.Parse()
+}
 
-	fmt.Printf("Action: \"%s\" \n", *action)
-
+func createConnection(address string) (conn *grpc.ClientConn, ctx context.Context, cancel context.CancelFunc) {
 	// Set up a connection to the server.
-	conn, err := grpc.Dial(*address, grpc.WithInsecure())
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
-	defer conn.Close()
 
-	c := v1.NewAuthenticationServiceClient(conn)
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	switch *action {
-	case "login":
-		doLogin(ctx, c, username)
-	case "signup":
-		doSignUp(ctx, c, username, email)
-	case "token":
-		doTokenValidate(ctx, c)
-	default:
-		log.Printf("Possible options [login, signup, token]\n")
-		return
-	}
+	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+	return conn, ctx, cancel
 }
 
-func doTokenValidate(ctx context.Context, c v1.AuthenticationServiceClient) {
+func doTokenValidate(ctx context.Context, conn *grpc.ClientConn) {
+	c := v1.NewAuthenticationServiceClient(conn)
+
 	req := v1.ValidateRequest{
 		Api:   apiVersion,
-		Token: getPassword(),
+		Token: getToken(),
 	}
 	res, err := c.Validate(ctx, &req)
 	if err != nil {
@@ -86,19 +121,20 @@ func doTokenValidate(ctx context.Context, c v1.AuthenticationServiceClient) {
 	log.Printf("Validate result: <%+v>\n\n", res)
 }
 
-func doSignUp(ctx context.Context, c v1.AuthenticationServiceClient, username *string, email *string) {
+func doSignUp(ctx context.Context, conn *grpc.ClientConn, username string, email string) {
+	c := v1.NewAuthenticationServiceClient(conn)
 	// Call Create
-	req1 := v1.SignUpRequest{
+	req := v1.SignUpRequest{
 		Api:      apiVersion,
-		Username: *username,
-		Email:    *email,
+		Username: username,
+		Email:    email,
 		Password: getPassword(),
 	}
-	res1, err := c.SignUp(ctx, &req1)
+	res, err := c.SignUp(ctx, &req)
 	if err != nil {
 		log.Fatalf("Create failed: %v", err)
 	}
-	log.Printf("Create result: <%+v>\n\n", res1)
+	log.Printf("Create result: <%+v>\n\n", res)
 }
 
 func getPassword() string {
@@ -106,19 +142,29 @@ func getPassword() string {
 	prompt := &survey.Password{
 		Message: "Please type your password",
 	}
-	survey.AskOne(prompt, &password)
+	survey.AskOne(prompt, &password, survey.WithValidator(survey.Required))
 	return password
 }
 
-func doLogin(ctx context.Context, c v1.AuthenticationServiceClient, username *string) {
-	req2 := v1.LoginRequest{
+func getToken() string {
+	token := ""
+	prompt := &survey.Password{
+		Message: "Please type user token",
+	}
+	survey.AskOne(prompt, &token, survey.WithValidator(survey.Required))
+	return token
+}
+
+func doLogin(ctx context.Context, conn *grpc.ClientConn, username string) {
+	c := v1.NewAuthenticationServiceClient(conn)
+	req := v1.LoginRequest{
 		Api:      apiVersion,
-		Username: *username,
+		Username: username,
 		Password: getPassword(),
 	}
-	res2, err := c.Login(ctx, &req2)
+	res, err := c.Login(ctx, &req)
 	if err != nil {
 		log.Fatalf("Login failed: %v", err)
 	}
-	log.Printf("Login result: <%+v>\n\n", res2)
+	log.Printf("Login result: <%+v>\n\n", res)
 }
