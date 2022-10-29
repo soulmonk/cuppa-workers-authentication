b635e69ee3567c2dd1313ee586a6b9e7c06d83be
Vitalii Melnykov <vitalii.melnykov@sumup.com>
2022-10-29 16:52:12 +0200

    in progress, sqlc, refresh token, new api refresh token, logout

---
+++ db/migration/1660557059891_add_refresh_token_table.sql
@@ -0,0 +1,16 @@
+alter table "user"
+  drop column refresh_token;
+
+drop table "user_userverification";
+
+create table "refresh_token"
+(
+  id         BIGSERIAL               not null
+    constraint refresh_token_pk
+      primary key,
+  user_id    bigint                 not null
+    references "user",
+  created_at timestamp default now() not null,
+  source     varchar(64)             not null,
+  token      uuid                    not null
+)
